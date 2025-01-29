992edbdf5d2e6fd64d8962b26f791467ba03a903
Itishjot Singh <168682348+OftenAFK@users.noreply.github.com>
2025-01-30 03:05:33 +0530

    Solution to Check Triangle Validity

---
+++ Check Triangle Validity@@6788dd63490637cfc44e1b93/code.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int arr[100];
+    int n= 0, num;
+
+    while(scanf("%i", &num)== 0)
+    {
+        arr[n++]= num;
+    }
+
+    if (arr[0] + arr[1] > arr[2])
+    {
+        printf("Valid\n")
+    }
+    else
+    {
+        printf("Invalid\n")
+    }
+
+    return 0;
+}
