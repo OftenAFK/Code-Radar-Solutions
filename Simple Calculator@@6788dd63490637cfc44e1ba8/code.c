bbfb421231aa69856d7929eed6dbbf75032913cf
Itishjot Singh <168682348+OftenAFK@users.noreply.github.com>
2025-02-04 15:29:23 +0530

    Solution to Simple Calculator

---
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int addition(int first, int second)
+{
+    int counter= first + second;
+    return counter;
+}
+
+int subtraction(int first, int second)
+{
+    int counter= first - second;
+    return counter;
+}
+
+int multiplication(int first, int second)
+{
+    int counter= first * second;
+    return counter;
+}
+
+int division()
