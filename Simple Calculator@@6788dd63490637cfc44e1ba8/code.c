80821a675d295cce472209bad358104135a9e298
Itishjot Singh <168682348+OftenAFK@users.noreply.github.com>
2025-02-04 15:45:03 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 int addition(int first, int second)
 {
@@ -18,4 +19,35 @@ int multiplication(int first, int second)
     return counter;
 }
 
-int division()
+int division(int first, int second)
+{
+    int counter= first / second;
+    return counter;
+}
+
+int main(void)
+{
+    int first, second;
+    char special_character;
+    printf("");
+    scanf("%i %i %c", &first, &second, &special_character);
+
+    if (strcmp(special_character, "+")== 0)
+    {
+        printf("%i", addition(first, second));
+    }
+    else if (strcmp(special_character, "-")== 0)
+    {
+        printf("%i", subtraction(first, second));
+    }
+    else if (strcmp(special_character, "*")== 0)
+    {
+        printf("%i", multiplication(first, second));
+    }
+    else
+    {
+        printf("%i", division(first, second));
+    }
+
+    return 0;
+}
