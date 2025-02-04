b4cbce57872e891d8b98bef932a7b0b60ca38f91
Itishjot Singh <168682348+OftenAFK@users.noreply.github.com>
2025-02-04 21:23:21 +0530

    Solution to Check Month Days

--- Check Month Days@@6788dd63490637cfc44e1baa/code.c
+++ Check Month Days@@6788dd63490637cfc44e1baa/code.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int month(month_number)
+int month(int month_number)
 {
     int names[12]= {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
     return names[month_number - 1];
