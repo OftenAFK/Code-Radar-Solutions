f8bc85a68fde58adbb2cc186be9a8f2f30e3602d
Itishjot Singh <168682348+OftenAFK@users.noreply.github.com>
2025-02-04 15:17:25 +0530

    Solution to Find Day of the Week

--- Find Day of the Week@@6788dd63490637cfc44e1ba7/code.c
+++ Find Day of the Week@@6788dd63490637cfc44e1ba7/code.c
@@ -2,7 +2,7 @@
 
 const char* weekday(int days)
 {
-    const char* week[7]= {Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday};
+    const char* week[7]= {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
     result= week[days-1];
 
     return result;
