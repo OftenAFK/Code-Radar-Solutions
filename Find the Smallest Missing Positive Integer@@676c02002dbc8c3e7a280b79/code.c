cd08ff7e13adff805bd4c659c8b17e11fc5b0319
Itishjot Singh <168682348+OftenAFK@users.noreply.github.com>
2025-02-25 12:31:09 +0530

    Solution to Find the Smallest Missing Positive Integer

--- Find the Smallest Missing Positive Integer@@676c02002dbc8c3e7a280b79/code.c
+++ Find the Smallest Missing Positive Integer@@676c02002dbc8c3e7a280b79/code.c
@@ -24,7 +24,7 @@ int sorting(int arr[], int length)
 int devchic(int arr[], int length)
 {   
     sorting(arr, length);
-    for (int i= 0; i< length; i++)
+    for (int i= 0; i< length- 1; i++)
     {
         if (arr[i] + 1 != arr[i+1])
         {
