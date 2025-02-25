ab3770b64b43b5f428ac7336c92d60c9775661ef
Itishjot Singh <168682348+OftenAFK@users.noreply.github.com>
2025-02-25 12:13:05 +0530

    Solution to Find the Maximum Product of Two Elements in an Array

--- Find the Maximum Product of Two Elements in an Array@@676c02002dbc8c3e7a280b80/code.c
+++ Find the Maximum Product of Two Elements in an Array@@676c02002dbc8c3e7a280b80/code.c
@@ -1,43 +1,40 @@
 #include <stdio.h>
+#include <limits.h>
 
 int max_product(int arr[], int length)
 {
-    int max1= arr[0], max2= arr[0];
-    int min1= arr[0], min2= arr[0];
-
-    for (int i= 0; i< length; i++)
+    if (length < 2) 
     {
-        if (arr[i]> max1)
-        {   
-            max2= max1;
-            max1= arr[i];
-        }
-        else if (max1 > arr[i] && arr[i]> max2)
+        return 0;
+    }
+
+    int max1 = INT_MIN, max2 = INT_MIN;
+    int min1 = INT_MAX, min2 = INT_MAX;
+
+    for (int i = 0; i < length; i++)
+    {
+        if (arr[i] > max1)
         {
-            max2= arr[i];
+            max2 = max1;
+            max1 = arr[i];
         }
-        else if (min1 > arr[i])
-        {   
-            min2= min1;
-            min1= arr[i];
-        }
-        else if (arr[i]> min1 && min2 > arr[i])
+        else if (arr[i] > max2)
         {
-            min2= arr[i];
+            max2 = arr[i];
+        }
+
+        if (arr[i] < min1)
+        {
+            min2 = min1;
+            min1 = arr[i];
+        }
+        else if (arr[i] < min2)
+        {
+            min2 = arr[i];
         }
     }
 
-    int max_result= max1 * max2;
-    int min_result= min1 * min2;
-
-    if (max_result > min_result)
-    {
-        return max_result;
-    }
-    else
-    {
-        return min_result;
-    }
+    return (max1 * max2 > min1 * min2) ? max1 * max2 : min1 * min2;
 }
 
 int main(void)
@@ -45,13 +42,18 @@ int main(void)
     int length;
     scanf("%i", &length);
 
+    if (length < 2)
+    {
+        return 1;
+    }
+
     int arr[length];
-    for (int i= 0; i< length; i++)
+    for (int i = 0; i < length; i++)
     {
         scanf("%i", &arr[i]);
     }
 
-    printf("%i", max_product(arr, length));
+    printf("%i\n", max_product(arr, length));
 
     return 0;
 }
