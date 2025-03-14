7792a7611a770a7b47134f45a83050cb6b18ef1a
Itishjot Singh <168682348+OftenAFK@users.noreply.github.com>
2025-03-14 16:27:26 +0530

    Solution to Find the Element with Maximum Frequency

--- Find the Element with Maximum Frequency@@676c02002dbc8c3e7a280b83/code.c
+++ Find the Element with Maximum Frequency@@676c02002dbc8c3e7a280b83/code.c
@@ -1,91 +1,126 @@
 #include <stdio.h>
+// #include <stdlib.h>
 
-int swap(int *first, int *second)
+int sorting(int arr[], int low, int high)
 {
-    int temp= *first;
-    *first= *second;
-    *second= temp;
-}
+    int pivot= arr[high];
+    int i= low - 1;
 
-void sorting(int arr[], int length)
-{
-    for (int i= 0; i< length- 1; i++)
+    for (int j= low; j< high; j++)
     {
-        for (int j= i+1; j< length; j++)
+        if (arr[j] < pivot)
         {
-            if (arr[i]> arr[j])
-            {
-                swap(&arr[i], &arr[j]);
-            }
+            i++;
+
+            int temp= arr[i];
+            arr[i]= arr[j];
+            arr[j]= temp;
         }
     }
+
+    int temp= arr[i+1];
+    arr[i+1]= arr[high];
+    arr[high]= temp;
+
+    return i+1;
+}
+
+void quicksort(int arr[], int low, int high)
+{
+    if (low < high)
+    {
+        int pivotIndex= sorting(arr, low, high);
+
+        quicksort(arr, pivotIndex + 1, high);
+        quicksort(arr, low, pivotIndex - 1);
+    }
+}
+
+int counting(int arr[], int length)
+{
+    int counter= 1;
+    for (int i= 0; i< length - 1; i++)
+    {
+        if (arr[i]!= arr[i+1])
+        {
+            counter++;
+        }
+    }
+
+    return counter;
+}
+
+int* removing_dups(int arr[], int length)
+{
+    int length_without_dups= counting(arr, length);
+
+    int* array_without_dups= (int* ) malloc (length_without_dups * sizeof (int)), temp= 0;
+
+    for (int i= 0; i< length - 1; i++)
+    {
+        if (arr[i]!= arr[i+1])
+        {
+            array_without_dups[temp]= arr[i];
+            temp++;
+        }
+    }
+
+    if (arr[length - 1]== arr[length - 2])
+    {
+        array_without_dups[length_without_dups - 1]= arr[length - 1];
+    }
+
+    return array_without_dups;
 }
 
 int max(int arr[], int length)
 {
-    int first= arr[0];
-    for (int i= 0; i< length; i++)
-    {
-        if (arr[i]> first)
-        {
-            first= arr[i];
-        }
-    }
-
-    return first;
-}
-
-void dups(int arr[], int length)
-{   
-    sorting(arr, length);
-
-    int j= 0;
-    for (int i= 1; i< length- 1; i++)
-    {
-        if (arr[i]!= arr[i-1])
-        {
-            arr[j]= arr[i];
-            j++;
-        }
-    }
-    arr[j]= arr[length - 1];
-    j++;
-}
-
-void frequency(int arr[], int length)
-{   
-    sorting(arr, length);
-
-    int freq[length];
-    for (int i= 0; i< length; i++)
-    {
-        freq[i]= 0;
-    }
-
-    int counter= 1;
+    int max= arr[0];
     for (int i= 1; i< length; i++)
-    {   
-        if (arr[i]== arr[i-1])
-        {
-            counter++;
-        }
-        else 
-        {
-            freq[i]= counter;
-            counter= 1;
-        }
-    }
-
-    int result= max(freq, length);
-    dups(arr, length);
-
-    for(int i= 0; i< length; i++)
     {
-        if (freq[i]== result)
+        if (arr[i] > max)
         {
-            printf("%i", arr[i]);
+            max= arr[i];
         }
     }
+
+    return max;
+}
+
+int frequency(int arr[], int length)
+{
+    quicksort(arr, 0, length - 1);
+    int length_without_dups= counting(arr, length);
+
+    int *temp_arr= removing_dups(arr, length), frequency[length_without_dups];
+
+    int count= 1, frequencyIndex= 0;
+    for (int i= 1; i< length; i++)
+    {
+        if (arr[i-1]== arr[i])
+        {
+            count++;
+        }
+        else
+        {
+            frequency[frequencyIndex++]= count;
+            count= 1;
+        }
+    }
+
+    int maxFrequency= max(frequency, length_without_dups), temp= 0;
+
+    for (int i= 0; i< length_without_dups; i++)
+    {
+        if (arr[i]== maxFrequency)
+        {
+            temp= i;
+            break;
+        }
+    }
+
+    int result= temp_arr[temp];
+    return result;
 }
 
 int main(void)
@@ -94,14 +129,13 @@ int main(void)
     scanf("%i", &length);
 
     int arr[length];
+
     for (int i= 0; i< length; i++)
     {
         scanf("%i", &arr[i]);
     }
 
-    frequency(arr, length);
+    printf("%i\n", frequency(arr, length));
 
     return 0;
 }
-
-
